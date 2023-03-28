867b4cce6296c19ae90cf506608bfe6af90e48e2
gastonmatias <g.matiasvillagra@gmail.com>
2023-03-28 17:50:25 -0300

    fix environment variables

--- db/connection.ts
+++ db/connection.ts
@@ -1,11 +1,21 @@
 //?https://sequelize.org/docs/v6/getting-started/
-
 import { Sequelize } from "sequelize";
 
-// connecting to db with method 3: Passing parameters separately
-const dbSequelize = new Sequelize('tsrestserver', 'postgres', 'oracle', {
-    host: 'localhost',
+import dotenv from 'dotenv';
+dotenv.config()
+
+const db_name = process.env.DB_NAME!
+const db_user = process.env.DB_USER!
+const db_pass = process.env.DB_PASSWORD!
+
+//* connecting to db with method 3: Passing parameters separately
+const dbSequelize = new Sequelize(db_name, db_user, db_pass, {
     dialect: 'postgres'
-  });
+});
+
+//*  metodo 2: con uri
+// const dbSequelize = new Sequelize(process.env.URI!,{
+//   dialect: 'postgres'
+// })
 
 export default dbSequelize
--- dist/db/connection.js
+++ dist/db/connection.js
@@ -1,11 +1,22 @@
 "use strict";
-//?https://sequelize.org/docs/v6/getting-started/
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+//?https://sequelize.org/docs/v6/getting-started/
 const sequelize_1 = require("sequelize");
-// connecting to db with method 3: Passing parameters separately
-const dbSequelize = new sequelize_1.Sequelize('tsrestserver', 'postgres', 'oracle', {
-    host: 'localhost',
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const db_name = process.env.DB_NAME;
+const db_user = process.env.DB_USER;
+const db_pass = process.env.DB_PASSWORD;
+//* connecting to db with method 3: Passing parameters separately
+const dbSequelize = new sequelize_1.Sequelize(db_name, db_user, db_pass, {
     dialect: 'postgres'
 });
+//*  metodo 2: con uri
+// const dbSequelize = new Sequelize(process.env.URI!,{
+//   dialect: 'postgres'
+// })
 exports.default = dbSequelize;
 //# sourceMappingURL=connection.js.map
